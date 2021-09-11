ba7b06e95635f3866d9e64687d26718889b79912
Walups <74693883+Walups@users.noreply.github.com>
2021-09-11 12:20:50 -0300

    Update aluno.cs

--- classes/aluno.cs
+++ classes/aluno.cs
@@ -2,9 +2,19 @@ using System;
 
 namespace IDontKnow{
 
-    public class Aluno : User{
+    public class Aluno
+    {
+        
+        public string name {get,set};
+        public string age {get,set};
+        public string email {get,set};
+        public double altura {get,set};
+        public double peso {get,set};
+        public double IMC = peso / altura;
 
+        private string senha;
 
+        public string senha {get => senha, set => senha = value; }
 
     }
 
